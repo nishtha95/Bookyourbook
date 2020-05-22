75e2e84ab5087b7badaa01f5058c439770ba21cb
nishtha95 <paliwal.nishtha@gmail.com>
2020-05-22 10:57:50 +0530

    init

---
+++ src/main/java/com/bookyourbook/BookyourbookApplication.java
@@ -0,0 +1,13 @@
+package com.bookyourbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyourbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookyourbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookyourbook/BookyourbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookyourbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookyourbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
